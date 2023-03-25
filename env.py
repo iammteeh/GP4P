41d8016c47c8a997fc2dc8959f20abc06c898139
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-03-25 15:26:35 +0100

    [ADD] env variables

--- env.py
+++ env.py
@@ -1,5 +1,7 @@
 import os 
 
 PWD = os.getcwd()
-DATADIR = PWD + '/data/'
-MODE = "simple" # to choose which structure is induced with the data
+DATADIR = PWD + '/rawdata/'
+MODELDIR = PWD + '/modeldata/'
+MODE = "not simple" # to choose which structure is induced with the data
+Y = "energy"
