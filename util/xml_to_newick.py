c47a882a770fc240ed252a1a82d60038e7b2ace2
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-04-09 21:30:55 +0200

    [FIX] xml to newick converter to be compatible with ete3

--- util/xml_to_newick.py
+++ util/xml_to_newick.py
@@ -1,15 +1,16 @@
 import xml.etree.ElementTree as ET
 
 def xml_to_newick(xml_string):
-    def newick_recursive(element, attribute="name"):
-        if len(element) == 0:  # Leaf node
-            name = element.text
-            attribute = element.attrib.get(attribute, "")
-            return f"{name}:{attribute}"
-        else:
-            children_newick = ",".join(newick_recursive(child) for child in element)
-            attribute = element.attrib.get(attribute, "")
-            return f"({children_newick}){attribute}"
+    def newick_recursive(element):
+        if element.tag == "configurationOption":
+            name = element.find("name").text.strip()
+            if len(element) == 1:  # Leaf node
+                return f"{name}"
+            else:
+                children_newick = ",".join(newick_recursive(child) for child in element.findall("./"))
+                return f"({children_newick}){name}"
+        return ""
 
     root = ET.fromstring(xml_string)
-    return newick_recursive(root) + ";"
+    newick_tree = newick_recursive(root) + ";"
+    return newick_tree
