b6a8fafaa555795a6a5688bf4d5187b6741b487a
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-04-12 13:47:51 +0200

    [ADD] sws env

--- env.py
+++ env.py
@@ -3,6 +3,7 @@ import os
 PWD = os.getcwd()
 DATADIR = PWD + '/rawdata/'
 MODELDIR = PWD + '/modeldata/'
+SWS = "LLVM_energy"
 MODE = "not simple" # to choose which structure is induced with the data
 X_val = bool
 Y = "energy"
