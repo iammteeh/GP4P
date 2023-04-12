df27e5a497fa20ccb065e94ac06b72a9e51bd2cc
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-04-12 13:11:52 +0200

    [ADD] symbolic regression

--- domain/regression.py
+++ domain/regression.py
@@ -22,6 +22,13 @@ class Regression:
             return linear_model.Ridge(alpha=0.01, max_iter=9999, copy_X=True, fit_intercept=True, normalize=False)
         elif method == "elastic":
             return linear_model.ElasticNet(alpha=0.01, max_iter=9999, copy_X=True, fit_intercept=True, normalize=False)
+        elif method == "symbolic":
+            return SymbolicRegressor(population_size=5000,
+                            generations=20, stopping_criteria=0.01,
+                            p_crossover=0.7, p_subtree_mutation=0.1,
+                            p_hoist_mutation=0.05, p_point_mutation=0.1,
+                            max_samples=0.9, verbose=1,
+                            parsimony_coefficient=0.01, random_state=42)
 
     def __enter__(self):
         return self
