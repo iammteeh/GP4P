e93d138dc837690b08c5ca57efc98e437083b61f
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-05-24 16:16:43 +0200

    [CHANGE] update env

--- domain/env.py
+++ domain/env.py
@@ -5,10 +5,11 @@ DATADIR = PWD + '/rawdata/'
 MODELDIR = PWD + '/modeldata/'
 SWS = "LLVM_energy"
 MODE = "not simple" # to choose which structure is induced with the data
-DUMMY_DATA = True
-X_type = float # for mcmc=float for probit=bool
+DUMMY_DATA = False
+X_type = float # for mcmc=float for probit/logistic=bool
+EXTRAFUNCTIONAL_FEATURES = True
 POLY_DEGREE = 3
 Y = "energy"
-REGRESSION = "PLS"
+REGRESSION = "LassoCV"
 REGRESSION_PENALTY = [0.1, 1, 10]
 COEFFICIENT_THRESHOLD = 3
