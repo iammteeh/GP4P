15c8b9c8f1e7ac04a541e950d9944a0d8e7605c4
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-05-24 16:16:16 +0200

    [ADD] pipeline for BayesSearchCV

---
+++ application/bayes_pipeline.py
@@ -0,0 +1,36 @@
+from adapters.preprocessing import prepare_dataset, preprocessing
+from application.bayesify_pipeline import bayesify_pipeline
+from sklearn.model_selection import train_test_split
+from domain.model import Model
+from domain.env import DUMMY_DATA
+from domain.regression import Regression
+from dp.bayes_search_cv import bayes_search_cv
+from domain.metrics import get_metrics
+import numpy as np
+try:
+    np.distutils.__config__.blas_opt_info = np.distutils.__config__.blas_ilp64_opt_info
+except Exception:
+    pass
+
+ds = prepare_dataset(DUMMY_DATA)
+feature_names = ds.get_feature_names() if not DUMMY_DATA else ds["feature_names"]
+X_train, X_test, y_train, y_test = preprocessing(ds, "2_poly", "none")
+
+print("Starting BayesSearchCV")
+opt = bayes_search_cv
+
+# plot the model
+
+# Fit the BayesSearchCV to your data
+opt.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = opt.predict(X_test)
+
+# Evaluate the model
+metrics = get_metrics(y_test, y_pred, type="regression")
+for metric in metrics:
+    print(f"{metric}: {metrics[metric]}")
+
+# Get the best parameters
+print('Best parameters found: ', opt.best_params_)
