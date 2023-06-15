7afeb7a96ea2f89b80a9f0aa0e4ed845a97185af
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-06-15 14:46:52 +0200

    [CHANGE] calculate regression penalty with logspace

--- domain/env.py
+++ domain/env.py
@@ -5,11 +5,13 @@ DATADIR = PWD + '/rawdata/'
 MODELDIR = PWD + '/modeldata/'
 SWS = "LLVM_energy"
 MODE = "not simple" # to choose which structure is induced with the data
-DUMMY_DATA = False
+DUMMY_DATA = True
 X_type = float # for mcmc=float for probit/logistic=bool
 EXTRAFUNCTIONAL_FEATURES = True
-POLY_DEGREE = 3
+POLY_DEGREE = 2
 Y = "energy"
 REGRESSION = "LassoCV"
-REGRESSION_PENALTY = [0.1, 1, 10]
+REGRESSION_PENALTY = (-2, 4, 50) # (start, stop, num)
+#ALPHAS = [0.1, 0.5, 1, 5, 10, 50, 100]
+ALPHAS = (-2, 3, 1000)
 COEFFICIENT_THRESHOLD = 3
