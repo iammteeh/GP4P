84b2b01a5fa8f781f6e94200345647feae2cf08c
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-06-19 13:51:31 +0200

    [CHANGE] some env vars

--- domain/env.py
+++ domain/env.py
@@ -5,7 +5,7 @@ DATADIR = PWD + '/rawdata/'
 MODELDIR = PWD + '/modeldata/'
 SWS = "LLVM_energy"
 MODE = "not simple" # to choose which structure is induced with the data
-DUMMY_DATA = True
+USE_DUMMY_DATA = True
 X_type = float # for mcmc=float for probit/logistic=bool
 EXTRAFUNCTIONAL_FEATURES = True
 POLY_DEGREE = 2
@@ -15,5 +15,5 @@ REGRESSION_PENALTY = (-2, 4, 50) # (start, stop, num)
 #ALPHAS = [0.1, 0.5, 1, 5, 10, 50, 100]
 ALPHAS = (-2, 3, 1000)
 COEFFICIENT_THRESHOLD = 3
-SAFE_FIGURES = True
-RESULTS_DIR = PWD + '/results/'
+SAVE_FIGURES = True
+RESULTS_DIR = PWD + '/evaluation/'
