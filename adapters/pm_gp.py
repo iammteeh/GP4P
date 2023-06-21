6827dd3dafeedfad4c688b1f064a41a3fccd657b
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-06-21 14:56:21 +0200

    [CHANGE] set cov_matrix to None for testing purpose

--- adapters/pm_gp.py
+++ adapters/pm_gp.py
@@ -6,12 +6,19 @@ def define_gp(X, µ_vector, cov_matrix, Y, prior=None, kernel=None, noise=None):
     ### override arguments for test purpose only
     kernel = "expquad"
     prior = None
+    cov_matrix = None
     ###
     # convert µ_vector to pm mean object
     mean_func = pm.gp.mean.Constant(c=np.mean(µ_vector))
     # create covariance object that can be used by pm
-    if not isinstance(cov_matrix, pm.gp.cov.ExpQuad):
-        cov_func = pm.gp.cov.ExpQuad(len(µ_vector), ls=1.0) if kernel is not None else tt.as_tensor_variable(cov_matrix)
+    if not isinstance(cov_matrix, pm.gp.cov.Covariance):
+        #cov_func = pm.gp.cov.ExpQuad(len(µ_vector), ls=1.0) if kernel is not None else tt.as_tensor_variable(cov_matrix)
+        # set cov params
+        c = 1.0
+        d = 2
+        offset = 1.0
+        tau = 0.1
+        cov_func = tau* pm.gp.cov.Polynomial(input_dim=len(X.T), c=c, d=d, offset=offset)
     gp = pm.gp.Latent(mean_func=mean_func, cov_func=cov_func if cov_func else cov_matrix) if noise is None else pm.gp.Marginal(mean_func=µ_vector, cov_func=cov_func)
     #f = gp.marginal_likelihood("f", X=µ_vector, y=prior, noise=0.1)
     #if not isinstance(prior, pm.MvNormal):
