af51cc1f543598130cb9a2809435885fc433ac55
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-06-22 16:31:26 +0200

    [CHANGE] rm deprecated function for calculating prior information

--- adapters/calculate_pretrained_priors.py
+++ adapters/calculate_pretrained_priors.py
@@ -80,115 +80,7 @@ class Priors:
 
         return reg_dict, err_dict
     
-    def get_prior_weighted_normal(self, X, y, feature_names, gamma=1, stddev_multiplier=3, kernel="expquad"):
-        print("Getting priors from lin regs.")
-        reg_dict_final, err_dict = self.get_regression_spectrum(X, y, feature_names)
-        all_raw_errs = [errs["raw"] for errs in list(err_dict.values())]
-        all_abs_errs = np.array(
-            [abs(err["y_pred"] - err["y_true"]) for err in all_raw_errs]
-        )
-        mean_abs_errs = all_abs_errs.mean(axis=1)
-        all_rel_errs = np.array(
-            [
-                abs((err["y_pred"] - err["y_true"]) / err["y_true"])
-                for err in all_raw_errs
-            ]
-        )
-        mean_rel_errs = all_rel_errs.mean(axis=1)
-        reg_list = list(reg_dict_final.values())
-        print(f"fitting {len(reg_list)} regressors")
-        print(f"reg_list: {reg_list}")
-
-        means_weighted = []
-        stds_weighted = []
-        weights = (
-            1 - MinMaxScaler().fit_transform(np.atleast_2d(mean_abs_errs).T).ravel()
-        )
-        err_mean, err_std = weighted_avg_and_std(mean_abs_errs, weights, gamma=gamma)
-        noise_sd_over_all_regs = err_mean + 3 * err_std
-        root_candidates = np.array([reg.intercept_ for reg in reg_list])
-        root_mean, root_std = weighted_avg_and_std(
-            root_candidates, weights, gamma=gamma
-        )
-
-        coef_matrix = []
-        for coef_id, coef in enumerate(feature_names):
-            coef_candidates = np.array([reg.coef_[coef_id] for reg in reg_list]) # vector of coefficients
-            print(f"coef_candidates with shape {len(coef_candidates)} {coef_candidates}")
-            coef_matrix.append(coef_candidates)
-            mean_weighted, std_weighted = weighted_avg_and_std(
-                coef_candidates, weights, gamma=gamma
-            )
-            means_weighted.append(mean_weighted)
-            stds_weighted.append(stddev_multiplier * std_weighted)
-
-        print(f"coef matrix: {coef_matrix}")
-        print(f"means weighted: {means_weighted}")
-        print(f"stds weighted: {stds_weighted}")
-
-        print(f"construct cov matrix")
-        coef_matrix = np.array(coef_matrix).T # convert to numpy array and transpose
-        coef_matrix = np.corrcoef(coef_matrix) # perturb the covariance matrix with the correlation matrix
-        if kernel == "linear":
-            cov_matrix = np.cov(coef_matrix, rowvar=False)  # Compute the covariance matrix with a linear kernel (i.e. no kernel) as its derived from the linear regression coefficients
-        elif kernel == "expquad":
-            cov_matrix = pm.gp.cov.ExpQuad(input_dim=len(X.T), ls=len(means_weighted)).full(X) # Compute the covariance matrix with a squared exponential kernel
-        elif kernel == "rbf":
-            cov_matrix = pm.gp.cov.RBF(1, coef_matrix.shape[1]).full(X) # Compute the covariance matrix with a radial basis function kernel
-        elif kernel == "matern":
-            cov_matrix = pm.gp.cov.Matern52(1, coef_matrix.shape[1]).full(X)
-        elif kernel == "X2":
-            cov_matrix = metrics.polynomial_kernel(coef_matrix, coef_matrix, degree=2)
-        #elif kernel == "X3":
-        #    cov_matrix = sklearn.metrics.pairwise.polynomial_kernel(coef_matrix, coef_matrix, degree=3)
-        #elif kernel == "chi2":
-        #    cov_matrix = sklearn.metrics.pairwise.chi2_kernel(coef_matrix, coef_matrix)
-        #elif kernel == "additive_chi2":
-        #    cov_matrix = sklearn.metrics.additive_chi2_kernel(coef_matrix, coef_matrix)
-        #elif kernel == "laplacian":
-        #    cov_matrix = sklearn.metrics.pairwise.laplacian_kernel(coef_matrix, coef_matrix)
-        print(f"cov matrix: {cov_matrix}")
-
-        # check iff any NaN values occur, replace them with 0
-        if np.isnan(means_weighted).any():
-            means_weighted = np.nan_to_num(means_weighted)
-        # check if covariance matrix is positive semi-definite
-        def is_positive_semi_definite(matrix):
-            return np.all(np.linalg.eigvals(matrix) >= 0)
-
-        #if not is_positive_semi_definite(cov_matrix) and not isinstance(cov_matrix, pm.gp.cov.ExpQuad):
-        #    print("Covariance matrix is not positive semi-definite. Adding noise.")
-        #    cov_matrix += np.eye(cov_matrix.shape[0]) * noise_sd_over_all_regs
-        #    if not is_positive_semi_definite(cov_matrix):
-        #        print("Covariance matrix is still not positive semi-definite. Compute PSD approximation.")
-        #        cov_matrix = sqrtm(cov_matrix)
-        #        cov_matrix = cov_matrix @ cov_matrix.T
-        #        if not is_positive_semi_definite(cov_matrix):
-        #            raise ValueError("Covariance matrix is not positive semi-definite.")
-        
-        # Use the multivariate normal distribution for the coefficients
-        #coef_prior = dist.MultivariateNormal(jnp.array(means_weighted), covariance_matrix=jnp.array(cov_matrix))  
-        weighted_errs_per_sample = np.average(
-            all_abs_errs, axis=0, weights=mean_abs_errs
-        )
-        weighted_rel_errs_per_sample = np.average(
-            all_rel_errs, axis=0, weights=mean_rel_errs
-        )
-
-        base_prior = dist.Normal(jnp.array(root_mean), jnp.array(root_std))
-        error_prior = dist.Exponential(jnp.array(err_mean))
-
-        return (
-            means_weighted,
-            cov_matrix,
-            #coef_prior,
-            base_prior,
-            error_prior,
-            weighted_errs_per_sample,
-            weighted_rel_errs_per_sample,
-        )
-
-    def get_weighted_normal_params(self, X, y, feature_names, gamma=1, stddev_multiplier=3, kernel="expquad"):
+    def get_weighted_normal_params(self, X, y, feature_names, gamma=1, stddev_multiplier=3):
         print("Getting priors from lin regs.")
         reg_dict_final, err_dict = self.get_regression_spectrum(X, y, feature_names)
         all_raw_errs = [errs["raw"] for errs in list(err_dict.values())]
@@ -270,3 +162,7 @@ class Priors:
                     
         elif kernel == "expquad":
             return pm.gp.cov.ExpQuad(len(µ_vector), ls=1.0)
+        elif kernel == "rbf":
+            cov_matrix = pm.gp.cov.RBF(1, coef_matrix.shape[1]).full(X) # Compute the covariance matrix with a radial basis function kernel
+        elif kernel == "matern":
+            cov_matrix = pm.gp.cov.Matern52(1, coef_matrix.shape[1]).full(X)
