58127ddca069877b6d6a5dade038bb0972910855
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-07-19 15:28:18 +0200

    [ADD] gp prior env vars

--- domain/env.py
+++ domain/env.py
@@ -15,5 +15,7 @@ REGRESSION_PENALTY = (-2, 4, 50) # (start, stop, num)
 #ALPHAS = [0.1, 0.5, 1, 5, 10, 50, 100]
 ALPHAS = (-2, 3, 1000)
 COEFFICIENT_THRESHOLD = 3
+MEAN_FUNC = "constant"
+KERNEL_TYPE = "linear"
 SAVE_FIGURES = True
 RESULTS_DIR = PWD + '/evaluation/'
