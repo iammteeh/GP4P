bd97008ed14260cf7ef63ed15b1d62124ea3ec3f
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-09-20 16:23:23 +0200

    [CHANGE] migrate to pymc4 and exclude model iteration

--- application/gp_pipeline.py
+++ application/gp_pipeline.py
@@ -1,18 +1,20 @@
-from domain.env import USE_DUMMY_DATA, EXTRAFUNCTIONAL_FEATURES, POLY_DEGREE, MEAN_FUNC, KERNEL_TYPE, KERNEL_STRUCTURE
+from domain.env import USE_DUMMY_DATA, EXTRAFUNCTIONAL_FEATURES, POLY_DEGREE, MEAN_FUNC, KERNEL_TYPE, KERNEL_STRUCTURE, MODELDIR
 import numpy as np
 from application.init_pipeline import init_pipeline, get_numpy_features
 from adapters.pymc.prior_construction import PM_GP_Prior
 from adapters.pymc.kernel_construction import get_additive_lr_kernel
 from adapters.pymc.pm_gp import define_gp, get_kronecker_gp
-from adapters.pymc.util import save_model, load_model
+#from adapters.pymc.util import save_model, load_model
+import pickle
 import pymc as pm
-from pymc import Model, sample, sample_posterior_predictive, find_MAP, traceplot, summary
+from pymc import Model, sample, sample_posterior_predictive, find_MAP, traceplot, summary, compute_log_likelihood
 from pymc import gp as GP
 from arviz import loo, waic
 from sklearn.metrics import mean_squared_error, r2_score
 from adapters.visualization import plot_dist, plot_gp_feature, plot
 import matplotlib.pyplot as plt
 import arviz as az
+import datetime
 
 def eval_gp(posterior_predictive_distribution, X_test, y_test):
     # Calculate mean and standard deviation
@@ -33,43 +35,39 @@ def main():
     # use ndarrays of X and y
     X_train, X_test, y_train, y_test = get_numpy_features(X_train, X_test, y_train, y_test)
 
-    # register models
-    KERNEL_TYPE = ["linear", "expquad", "matern52"]
-    KERNEL_STRUCTURE = ["simple"]
+    with Model() as model:
+        # apply prior knowledge to gp
+        if KERNEL_TYPE == "linear":
+            f, gp, y_obs = define_gp(X_train, y_train, feature_names, mean_func=MEAN_FUNC, kernel=KERNEL_TYPE, structure=KERNEL_STRUCTURE)
+        else:
+            gp, y_obs = define_gp(X_train, y_train, feature_names, mean_func=MEAN_FUNC, kernel=KERNEL_TYPE, structure=KERNEL_STRUCTURE)
 
-    traces = []
-    for kernel_structure in KERNEL_STRUCTURE:
-        for kernel_type in KERNEL_TYPE:
+        # execute inference and sampling
+        mp = find_MAP(method="BFGS") # deprecated
+        inference_data = sample(1000)
+        inference_data.extend(sample_posterior_predictive(trace=inference_data, model=model))
+        inference_data.extend(compute_log_likelihood(inference_data, model=model))
+        
+        # save InferenceData object for later purposes
+        inference_data_filename = MODELDIR + f"PMGP_{MEAN_FUNC}_{KERNEL_TYPE}_{KERNEL_STRUCTURE}_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.tc"
+        inference_data.to_netcdf(inference_data_filename)
+        # test inference data
+        print(f"test inference data: {inference_data}")
+        print(f"posterior inference data: {inference_data.posterior}")
+        print(f"log likelihood: {inference_data.log_likelihood}")
+        #print(f"test trace: {inference_data.posterior['f'].values}") if kernel_type == "linear" else print(f"test trace: {inference_data.posterior['y_obs'].values}")
+        print(f"sample_stats: {inference_data.sample_stats}")
+        print(f"observed data: {inference_data.observed_data}")
+        #print(f"test posterior predictive: {post_pred}")
+        # score gp ONLY IF LOGLIKELIHOOD IS AVAILABLE
+        waic_score = waic(inference_data, pointwise=True)
+        loo_score = loo(inference_data, pointwise=True)
+        print(f"waic: {waic_score}")
+        print(f"loo: {loo_score}")
 
-            with Model() as model:
-                # apply prior knowledge to gp
-                if kernel_type == "linear":
-                    f, gp, y_obs = define_gp(X_train, y_train, feature_names, mean_func=MEAN_FUNC, kernel=kernel_type, structure=kernel_structure)
-                else:
-                    gp, y_obs = define_gp(X_train, y_train, feature_names, mean_func=MEAN_FUNC, kernel=kernel_type, structure=kernel_structure)
+        #ppc(post_pred, y_test)
+        #az.plot_ppc(az.from_pymc3(posterior_predictive=post_pred, model=model))
+        #plot(title=f"GP_PPC_{MEAN_FUNC}_{KERNEL_TYPE}")
 
-                mp = find_MAP(method="BFGS")
-                trace = sample(draws=1000, cores=1, trace=True, return_inferencedata=False)
-                saved_trace = pm.save_trace(trace)
-                traces.append(trace)
-                print(f"feature names: {feature_names}")
-                post_pred = sample_posterior_predictive(trace=trace, model=model, var_names=['y_obs'], samples=1000)
-                save_model(model, saved_trace, X_train)
-
-                # score gp
-                waic_score = waic(trace, model)
-                loo_score = loo(trace, model)
-                print(f"waic: {waic_score}")
-                print(f"loo: {loo_score}")
-
-                #ppc(post_pred, y_test)
-                az.plot_ppc(az.from_pymc3(posterior_predictive=post_pred, model=model))
-                plot(title=f"GP_PPC_{MEAN_FUNC}_{KERNEL_TYPE}")
-
-    # Plot convergence
-    for i, (structure, kernel) in enumerate(zip(KERNEL_STRUCTURE, KERNEL_TYPE)):
-        az.plot_trace(traces[i])
-        plt.title(f"Convergence plot for kernel: {type(kernel).__name__}")
-        plt.show()
 if __name__ == "__main__":
     main()
