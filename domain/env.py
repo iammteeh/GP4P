88a9ac94fa53180bf08dafafd6327b9beab40d9b
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-10-14 14:52:41 +0200

    [CHANGE] structure of env vars and add some comments

--- domain/env.py
+++ domain/env.py
@@ -6,24 +6,28 @@ MODELDIR = PWD + '/modeldata/'
 RESULTS_DIR = PWD + '/evaluation'
 SWS = "LLVM_energy"
 MODE = "not simple" # to choose which structure is induced with the data
-USE_DUMMY_DATA = True
-DATA_SLICE_MODE = "proportion"
-DATA_SLICE_PROPORTION = 1/7
-DATA_SLICE_AMOUNT = 1000
+USE_DUMMY_DATA = False
+DATA_SLICE_MODE = "amount"
+DATA_SLICE_PROPORTION = 1/1
+DATA_SLICE_AMOUNT = 10000
 X_type = bool # default = bool for one-hot encoded numerical features, else use float
+Y = "energy"
+# for linear regression pipelines #
 EXTRAFUNCTIONAL_FEATURES = False
 POLY_DEGREE = 2
-Y = "energy"
 REGRESSION = "LassoCV"
 REGRESSION_PENALTY = (-2, 4, 50) # (start, stop, num)
 #ALPHAS = [0.1, 0.5, 1, 5, 10, 50, 100]
 ALPHAS = (-2, 3, 1000)
 COEFFICIENT_THRESHOLD = 3
-GP_MODE = "marginal"
+# for GP pipelines #
 MEAN_FUNC = "linear_weighted"
 KERNEL_TYPE = "matern52"
 KERNEL_STRUCTURE = "simple"
-JAX = False
-NUTS_SAMPLER = "pymc"
+GP_MODE = "latent" # "latent" or "marginal" (PyMC)
 ARD = False
+# PyMC sample strategies #
+JAX = True
+NUTS_SAMPLER = "numpyro"
+###
 SAVE_FIGURES = True
