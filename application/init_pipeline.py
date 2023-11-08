02c809438c7f6f6a9938f6f3fe54ff7db1a097c6
Immanuel Thoke <immanuel.thoke@gmail.com>
2023-11-08 20:48:15 +0100

    [ADD] get tensor features function

--- application/init_pipeline.py
+++ application/init_pipeline.py
@@ -1,5 +1,6 @@
 from domain.env import USE_DUMMY_DATA
 from adapters.preprocessing import prepare_dataset, preprocessing
+from torch import tensor
 
 def init_pipeline(use_dummy_data=USE_DUMMY_DATA, extra_features="2_poly", scaler="minmax"):
     ds = prepare_dataset(use_dummy_data)
@@ -15,4 +16,11 @@ def get_numpy_features(X_train, X_test, y_train, y_test):
     X_test = X_test[1]
     y_train = y_train[1]
     y_test = y_test[1]
+    return X_train, X_test, y_train, y_test
+
+def get_tensor_features(X_train, X_test, y_train, y_test):
+    X_train = tensor(X_train).double()
+    X_test = tensor(X_test).double()
+    y_train = tensor(y_train).double().unsqueeze(-1) # make sure y is a column vector
+    y_test = tensor(y_test).double().unsqueeze(-1) # make sure y is a column vector
     return X_train, X_test, y_train, y_test
