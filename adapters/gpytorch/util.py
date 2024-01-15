e8d682ee4efee66ebc7c6c0afd4386bf6b6c926a
Immanuel Thoke <immanuel.thoke@gmail.com>
2024-01-15 13:40:24 +0100

    [ADD] group RATE

--- adapters/gpytorch/util.py
+++ adapters/gpytorch/util.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 from scipy.stats import median_abs_deviation, norm
+from domain.feature_model.boolean_masks import get_literals_and_interaction, get_opposites_and_interactions
+from time import time
 
 def decompose_matrix(X):
     # if rank(X)) < n_features, then X = U * S * V.T
@@ -16,6 +18,9 @@ def decompose_matrix(X):
     else:
         print(f"X is full rank and square. Rank(X) = {rank_X}")
         return U, S, V
+    
+def cross_decompose_matrices(X, Y):
+    pass
     
 def compute_principal_components(X, top_k=2):
     U, S, V = decompose_matrix(X)
@@ -37,12 +42,6 @@ def compute_pcs(X, top_k=10):
     U, S, V = torch.linalg.svd()
     PCs = U @ torch.diag(S) # multiply the eigenvalues by the eigenvectors
     return PCs[:, :top_k]
-        
-def eigenvalue_decomposition(K):
-    """
-    Decompose a kernel matrix into its eigenvalues and eigenvectors.
-    """
-
 
 def decompose_covariance(cov):
     # convert covariance matrix to numpy
@@ -65,14 +64,90 @@ def decompose_covariance(cov):
     
     return L
 
+def map_inverse_to_sample_feature(lam, B, theta):
+    """
+    GPT prompt: for given lambda and theta, find the corresponding beta
+    """
+    # compute beta
+    beta = B @ theta.T
+    # compute lambda
+    lam = beta @ beta.T
+    return beta, lam
+
+def get_beta(B, thetamat):
+    """
+    translated from https://github.com/lorinanthony/BAKR/blob/master/Rcpp/BAKRGibbs.cpp
+    
+    """
+    betamat = np.zeros((B.shape[0], thetamat.shape[0]))
+    for i in range(betamat.shape[1]):
+        if i % 100 == 0:
+            print(f"i = {i}")
+            print(f"B.shape = {B.shape}")
+            print(f"thetamat.shape = {thetamat.shape}")
+            print(f"betamat[:, i] = {betamat[:, i]}")
+            print(f"betamat[:, i].shape = {betamat[:, i].shape}")
+            print(f"thetamat[i, :] = {thetamat[i, :]}")
+            print(f"thetamat[i, :].shape = {thetamat[i, :].shape}")
+        betamat[:, i] = B @ thetamat[i, :].T
+    return betamat.T
+
+
 def get_thetas(K, q):
     U, S, V = decompose_matrix(K)
     #thetamat = U @ np.diag(S)
-    thetas = torch.diag(S) @ U.T
+    thetas = torch.diag(S) @ U.T # is the mean of the posterior distribution
     return thetas[:, :q] # return first q columns
     #theta_q = theta[:, q]
     #theta_minus_q = theta[:, q:]
     #return theta_q, theta_minus_q
+
+def get_groups(X, feature_group):
+    X = np.array(X)
+    j, minus_j = get_opposites_and_interactions(X, feature_group)
+    print(f"interactions: {minus_j}")
+    # get intersection
+    groups = np.array([x for x in set(map(tuple, X)) & set(map(tuple, minus_j))])
+    # Get indices of intersection in X and flatten them
+    idx = np.array([np.where((X == group).all(axis=1))[0][0] for group in groups])
+    print(f"minus_j intersects in X at: {idx}")
+    return idx, groups
+
+def group_RATE(mus, U, groups, nullify=None):
+    """
+    source https://github.com/lorinanthony/RATE/blob/master/Software/rate-bnn/rate/rate_bnn.py
+    example for a group of features:
+    group = np.array([1, 2, 3])
+    """
+    print("calculating group RATE (can take a while)...")
+    start = time()
+    # compute Lambda as the covariance matrix of thetas, i.e. Lambda = U * S * U.T using the cross-product matrix
+    Lambda = U @ U.T
+    J = np.arange(Lambda.shape[0]) # J = {1, ..., p}
+
+    if nullify:
+        J = np.delete(J, nullify, axis=0)
+    
+    def group_kld(group, idx):
+        if nullify:
+            j = np.array(np.unique(np.concatenate([group, nullify])))
+        else:
+            j = np.array(np.unique(group)) # j = {j_1, ..., j_q}
+        mu_j = mus[j]
+        Lambda_minus_j = np.delete(Lambda, j, axis=0)[:, j] # delete j-th row and j-th column
+
+        alpha_j = torch.matmul(Lambda_minus_j.T, torch.linalg.lstsq(np.delete(np.delete(Lambda, j, axis=0), j, axis=1), Lambda_minus_j, rcond=None)[0])
+
+        if nullify is None:
+            return 0.5 * alpha_j * mu_j ** 2
+        else:
+            return 0.5 * torch.matmul(torch.matmul(mu_j.T, alpha_j), mu_j)
+    
+    KLD = [group_kld(group, idx) for idx, group in enumerate(groups)]
+    end = time()
+    print(f"took {end - start:.2f} seconds to calculate group RATE")
+    return torch.tensor(KLD) / torch.sum(torch.tensor(KLD))
+
 
 def get_alphas(K, q):
     thetas = get_thetas(K, q)
@@ -112,25 +187,6 @@ def get_alphas_(mvn, X, index_map=None, low_rank=False):
         return alpha_values
     else:
         raise NotImplementedError("TODO: implement index_map")
-
-def get_beta(B, thetamat):
-    """
-    translated from https://github.com/lorinanthony/BAKR/blob/master/Rcpp/BAKRGibbs.cpp
-    
-    """
-    betamat = np.zeros((B.shape[0], thetamat.shape[0]))
-    for i in range(betamat.shape[1]):
-        if i % 100 == 0:
-            print(f"i = {i}")
-            print(f"B.shape = {B.shape}")
-            print(f"thetamat.shape = {thetamat.shape}")
-            print(f"betamat[:, i] = {betamat[:, i]}")
-            print(f"betamat[:, i].shape = {betamat[:, i].shape}")
-            print(f"thetamat[i, :] = {thetamat[i, :]}")
-            print(f"thetamat[i, :].shape = {thetamat[i, :].shape}")
-        betamat[:, i] = B @ thetamat[i, :].T
-    return betamat.T
-
 
 def get_betas(cov_matrix, Xs, index_map=None):
     X_pinv = torch.pinverse(Xs)
