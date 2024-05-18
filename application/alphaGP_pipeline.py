aa572c71592ac766670f8cfb7dcd472ad65a0d43
Immanuel Thoke <immanuel.thoke@gmail.com>
2024-05-18 15:58:52 +0200

    [WIP] new thesis pipeline

--- application/alphaGP_pipeline.py
+++ application/alphaGP_pipeline.py
@@ -1,28 +1,14 @@
-import gpytorch
-import torch
-import pyro
-from adapters.gpytorch.gp_model import ExactGP, SAASGP
-from gpytorch.likelihoods import GaussianLikelihood, StudentTLikelihood, DirichletClassificationLikelihood
-from gpytorch.mlls import ExactMarginalLogLikelihood, InducingPointKernelAddedLossTerm, VariationalELBO, GammaRobustVariationalELBO, PredictiveLogLikelihood
-from application.init_pipeline import init_pipeline, get_numpy_features
-from botorch import fit_fully_bayesian_model_nuts, fit_gpytorch_mll
-from domain.env import USE_DUMMY_DATA, MODELDIR, EXTRAFUNCTIONAL_FEATURES, POLY_DEGREE, MEAN_FUNC, KERNEL_TYPE, KERNEL_STRUCTURE, ARD, RESULTS_DIR
 import numpy as np
+import torch
+from adapters.gpytorch.gp_model import MyExactGP, SAASGP
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from adapters.preprocessing import prepare_dataset
+from application.init_pipeline import init_pipeline, yield_experiments
+from application.gpytorch_pipeline import fit_gpytorch_mll
+from adapters.gpytorch.pyro_model import fit_fully_bayesian_model_nuts
+from domain.env import USE_DUMMY_DATA, MODELDIR, EXTRAFUNCTIONAL_FEATURES, POLY_DEGREE, MEAN_FUNC, KERNEL_TYPE, KERNEL_STRUCTURE, ARD, RESULTS_DIR
 import datetime
 from time import time
-
-def waic(model, likelihood, X, Y):
-    model.eval()
-    with torch.no_grad():
-        output = model(X)
-        predictive_mean = output.mean
-        predictive_var = output.variance
-        error = Y - predictive_mean
-        log_likelihoods = -0.5 * torch.log(2 * np.pi * predictive_var) - 0.5 * (error**2) / predictive_var
-        lppd = torch.sum(torch.log(torch.mean(torch.exp(log_likelihoods), dim=0)))
-        p_waic = torch.sum(torch.var(log_likelihoods, dim=0))
-        waic = -2 * (lppd - p_waic)
-    return waic.item()
 
 def locate_invalid_data(data):
     if isinstance(data, torch.Tensor):
@@ -42,64 +28,73 @@ def validate_data(*args):
             raise ValueError("Data contains NaN or inf values.")
     print(f"data is fine.")
 
-def get_data():
-    ds, feature_names, X_train, X_test, y_train, y_test = init_pipeline(use_dummy_data=USE_DUMMY_DATA)
-    print(f"fit model having {X_train.shape[1]} features: {feature_names}")
-    rank = np.linalg.matrix_rank(X_train)
+def get_data(training_sizes=[100]):
+    ds = prepare_dataset()
+    # fetch data with the desired training size
+    data = yield_experiments(ds, training_size=training_sizes)
+    try:
+        while True:
+            feature_names, X_train, X_test, y_train, y_test = next(data)
 
-    # transform test data to tensor
-    X_test = torch.tensor(X_test).float()
-    y_test = torch.tensor(y_test).float()
+            yield (X_train, X_test, y_train, y_test, feature_names)
+    except StopIteration:
+        print("Finished fetching data.")
+        return
 
-    return (X_train, X_test, y_train, y_test, feature_names)
-
-def choose_model(model="GP", data=None):
-    if not data:
-        X_train, X_test, y_train, y_test, feature_names = get_data()
-    else:
+def choose_model(inference="MCMC", mean_func=MEAN_FUNC, kernel_type=KERNEL_TYPE, kernel_structure=KERNEL_STRUCTURE, data=None):
+    if data:
         X_train, X_test, y_train, y_test, feature_names = data
-    
-    if model == "GP":
-        return ExactGP(X_train, y_train, feature_names, likelihood="gaussian", kernel=KERNEL_TYPE, mean_func=MEAN_FUNC, structure=KERNEL_STRUCTURE)
-    elif model == "SAASGP":
-        return SAASGP(X_train, y_train, feature_names)
+    else:
+        raise ValueError("Data is required.")
+    if inference == "exact":
+        model = MyExactGP(X_train, y_train, feature_names, likelihood="gaussian", kernel=KERNEL_TYPE, mean_func=MEAN_FUNC, structure=KERNEL_STRUCTURE)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+        model_params = set(model.hyperparameters())
+        hyperparameter_optimizer = torch.optim.Adam([
+            {'params': list(model_params)},
+            {'params': [p for p in model.likelihood.parameters() if p not in model_params]}, # add only likelihood parameters that are not already in the list
+        ], lr=0.01)
+        mll = ExactMarginalLogLikelihood(model.likelihood, model)
+        return model, optimizer, mll, hyperparameter_optimizer
+    elif inference == "MCMC":
+        model = SAASGP(X_train, y_train, feature_names, mean_func="constant", kernel_structure=KERNEL_STRUCTURE, kernel_type=KERNEL_TYPE)
+        return model, (None)
     else:
         raise ValueError(f"Model {model} not found.")
 
 def main():
-    GP = "GP"
-    # init model
-    data = get_data()
-    X_train, X_test, y_train, y_test, feature_names = data
-    model = choose_model(model=GP, data=data)
-    mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    training_sizes = [50]#, 100, 200, 500, 1000]
+    kernel_types = ["RBF"]#, "matern52", "spectral_mixture", "RFF"]
+    kernel_structure = ["simple"]#, "additive"]
+    inference_methods = ["exact", "MCMC"]
+    i = 0
+    for data in get_data(training_sizes=training_sizes):
+        training_size = training_sizes[i]
+        for struct in kernel_structure:
+            for kernel_type in kernel_types:
+                for inference_type in inference_methods:
+                    X_train, X_test, y_train, y_test, feature_names = data
+                    print(f"fit model having {X_train.shape[1]} features: {feature_names}")
+                    model, *context = choose_model(inference=inference_type, data=data)
+                    # check for NaN / inf
+                    validate_data(model.X, X_test, model.y, y_test)
 
-    # check for NaN / inf
-    validate_data(model.X, X_test, model.y, y_test)
+                    # Fit model
+                    start = time()
+                    if inference_type == "exact":
+                        optimizer, mll, hyperparameter_optimizer = context
+                        fit_gpytorch_mll(model, optimizer, mll, hyperparameter_optimizer)
+                    elif inference_type == "MCMC":
+                        fit_fully_bayesian_model_nuts(model, jit_compile=True)
+                    end = time() - start
+                    print(f"Training time: {end:.2f}s")
 
-    # fit
-    fit_gpytorch_mll(mll, options={"maxiter": 1000, "disp": True})
-
-    # Evaluate model
-    model.eval()
-    model.likelihood.eval()
-
-    with torch.no_grad():
-        observed_pred = model.likelihood(model(X_test))
-        mean = observed_pred.mean
-        var = observed_pred.variance
-        posterior = model.posterior(X_test)
-        print(model.kernel.base_kernel.lengthscale.median())
-        print(posterior.mean.shape)
-        print(posterior.variance.shape)
-        print(f"Ground truth:     {y_test.squeeze(-1)}")
-        print(f"Mixture mean:     {posterior.mean.squeeze(-1)}")
-
-    #print(waic(model, model.likelihood, X_test, y_test))
-
-    # Save model
-    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    torch.save(model.state_dict(), f"{MODELDIR}/GPY_{MEAN_FUNC}_{KERNEL_TYPE}_{KERNEL_STRUCTURE}_ARD={ARD}__{timestamp}.pth")
-
+                    # set evaluation mode
+                    model.eval()
+                    model.likelihood.eval()
+                    # Save model
+                    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+                    torch.save(model.state_dict(), f"{MODELDIR}/{inference_type}_{kernel_type}_{kernel_structure}_{training_size}_{timestamp}.pth")
+                    i += 1
 if __name__ == "__main__":
     main()
