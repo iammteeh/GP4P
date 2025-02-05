c5f96befe236d6a5399690f2f2271a3453b7de9b
Immanuel Thoke <immanuel.thoke@uni-leipzig.de>
2025-02-05 16:23:32 +0100

    [FIX] typo

--- application/SAASGPJAX_pipeline.py
+++ application/SAASGPJAX_pipeline.py
@@ -71,7 +71,7 @@ def main():
     test_prior = model.pyro_sample_from_prior()
 
     # fit
-    with profiler.trace(LOGDIR/f"trace_{TIMESTAMP}", create_perfetto_link=True):
+    with profiler.trace(LOGDIR + f"/trace_{TIMESTAMP}.prof", create_perfetto_link=True):
         model.train()
         #TODO: add ENV for sampling parameters
         fit_fully_bayesian_model_nuts(model)
